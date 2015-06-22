d187a3c81e0940f2d0d595194338b1a3231f7eb1
rozzr <ronan.machugh@gmail.com>
2015-06-22 21:45:38 +0000

    npm installs done

--- app.js
+++ app.js
@@ -2,41 +2,13 @@ var express = require('express')
 var app = express()
 var server = require('http').Server(app)
 
-var io = require('socket.io')(server)
-var cylon = require('cylon')
+var io = require('socket.io')(server) // pass our http server to Socket.IO
 
 app.use(express.static(__dirname + '/public'))
 server.listen(8080)
 
-// this will be called when Cylon is fully initialized, which is when we should open up our WebSocket connection.
-var cylonReady = function(my) {
-  io
-    .of('/soundsocket')
-    .on('connection', function (socket) {
-      registerSocketHandlers(my, socket);
-      console.log('client connected') 
-      socket.emit('test_message', 'some data')
-    })
-}
-
-// this will be called each time a socket is opened, so each client will receive their own events when buttons are pushed.
-var registerSocketHandlers = function(my, socket) {
-  my.button.on('push', function() {
-    socket.emit('button', 'push')
+io
+  .of('/soundsocket') // establish a route to connect to using the Socket.IO client
+  .on('connection', function (socket) {
+	console.log('client connected') // each time a client establishes a connection, log a message to the console that the app is running on (on the Edison).
   })
-
-  my.button.on('release', function() {
-    socket.emit('button', 'release')
-  })
-}
-
-cylon.robot({
-  connections: {
-		edison: { adaptor: 'intel-iot' }
-	}
-  devices: {
-		button: { driver: 'button', pin: 2 }
-	}
-}).on('ready', cylonReady);
-
-cylon.start()
